56e615885c1e2e9e61cdf90ea59c4c47ab1aebeb
Boon <doctorpot1@hotmail.com>
2020-09-15 21:22:00 +0800

    duplicate init structure

---
+++ config.py
@@ -0,0 +1,16 @@
+sandbox = { "security": False }
+
+test = {
+	"appId": "STG2-SEQR-SELF-TEST",
+	"privateKeyPath": "./cert/stg-myinfo-2018.pem",
+	"publicCertPath": "./cert/pub.stg.consent.myinfo.gov.sg.cer",
+	"security": True
+}
+
+production = {
+	"appId": "XXX",
+	"privateKeyPath": "XXX",
+	"publicCertPath": "XXX",
+	"security": True
+}
+
---
+++ index.py
@@ -0,0 +1,32 @@
+import json
+import sys
+
+import config
+from lib import api
+
+# Testing data - START
+# NOTE
+# venueId is the unique id of the SafeEntry QR venue used to checkin/checkout
+# Staging - Example for Venue with no destination configured
+data = {
+    "subType": "uinfin",
+    "actionType": "checkin",
+    "sub": "S9960846C",
+    "venueId": "STG-180000001W-83338-SEQRSELFTESTSINGLE-SE",
+    "mobileno": "92376345"
+}
+
+# Staging - Example for Venue with multiple destinations (e.g. Lobby, Swimming Pool, Restaurant)
+# data = {
+# 	"subType": "uinfin",
+# 	"actionType": "checkin",
+# 	"sub": "S9960846C",
+# 	"venueId": "STG-180000001W-409531-SEQRSELFTESTMULTIPLE-SE",
+# 	"mobileno": "92376345",
+# 	"tenantId": "VENUE1"
+# };
+
+# Testing data - END
+
+if __name__ == '__main__':
+	apiType = str(sys.argv[1])
